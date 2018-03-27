006b4520c60aaab7326e8f43d64b508089a2e082
Giancarlo Anemone <ganemone@uber.com>
2018-03-27 16:58:45 -0700

    Initial implementation

---
+++ src/__tests__/request.node.js
@@ -0,0 +1,14 @@
+import App from 'fusion-react';
+import React from 'react';
+import {getSimulator} from 'fusion-test-utils';
+import test from 'tape-cup';
+import HelmetPlugin from '../index.js';
+
+test('Non render request', async t => {
+  const app = new App('test', el => el);
+  app.register(HelmetPlugin);
+  const sim = getSimulator(app);
+  const ctx = await sim.request('/');
+  t.equal(ctx.element, null, 'does not wrap ctx.element');
+  t.end();
+});
