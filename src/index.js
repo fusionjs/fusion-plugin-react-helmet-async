5f720dd73ddda80d6955eff2762567f52682af26
Kevin Grandon <kevingrandon@yahoo.com>
2019-04-11 20:57:54 -0700

    Revert "Export HelmetProvider along with Helmet + Plugin"
    This reverts commit a1357df8ae8866eb0504ea44a33257637b56eaa1.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@
  * @flow
  */
 
-import {Helmet, HelmetProvider} from 'react-helmet-async';
+import Helmet from 'react-helmet-async';
 import serverPlugin from './server';
 import clientPlugin from './browser';
 
 declare var __NODE__: Boolean;
 export default (__NODE__ ? serverPlugin : clientPlugin);
-export {Helmet, HelmetProvider};
+export {Helmet};
